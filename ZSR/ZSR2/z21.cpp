ba37ab7493bdc7f93549fb2ece7402e713b71c5a
HMByteSensei <127348494+HMByteSensei@users.noreply.github.com>
2024-01-21 00:51:02 +0100

    Function for minimum digit vector
    Function that takes vector of integers as parameter and returns vector with smallest digit from every element. 
    For example, if the function is given a vector with elements 32, 459, 72, 8, 24, 771, and 13, it will return a vector with elements 2(because 2 is smaller that 3), 4(because 4 is smallest digit in number 459), 2, 8, 2, 1, and 1.

---
+++ ZSR/ZSR2/z21.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+std::vector<int> NajmanjeCifre(std::vector<int> vektor) {
+    std::vector<int> rezultat;
+    for(int x : vektor) {
+        int min_br{10};
+        while(x != 0) {
+            int privremeni = x % 10;
+            x /= 10;
+            if(privremeni < min_br) {
+                min_br = privremeni;
+            }
+        }
+        rezultat.push_back(min_br);
+    }
+    return rezultat;
+}
+
+int main() {
+    std::cout << "Unesite prirodan broj n: ";
+    int n;
+    std::cin >> n;
+    if(std::cin.fail()) {
+        std::cin.clear(); 
+        std::cin.ignore(1000, '\n');
+        std::exit(0);
+    }
+    std::vector<int> vektor(n);
+    for(int i=0; i<n; i++) {
+        double element;
+        while(!(std::cin >> element) || static_cast<int>(element) != element) {
+            std::cout << "Pogresan unos!" << "\n";
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
+        vektor.at(i) = element;
+    }
+    for(int i=0; i<n; i++) std::cout << "Trazeni element je: " << NajmanjeCifre(vektor).at(i) << std::endl;;
+    return 0;
+}
