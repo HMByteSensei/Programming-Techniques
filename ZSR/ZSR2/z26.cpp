ccdf5e108ac68639dc024035445ddf4ce995a075
HMByteSensei <127348494+HMByteSensei@users.noreply.github.com>
2024-01-28 21:05:25 +0100

    z26 final commit

---
+++ ZSR/ZSR2/z26.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+std::vector<bool> Suma5UzastopnihBrojeva(std::vector<int> vektor) {
+    std::vector<bool> rezultat(vektor.size());
+    for(int i=0; i<vektor.size(); i++) {
+        if((vektor.at(i) % 5) == 0) {
+            rezultat.at(i) = true;
+        } else rezultat.at(i) = false;
+    }
+    return rezultat;
+}
+int main() {
+    std::cout << "Unesite n: ";
+    int n;
+    std::cin >> n;
+    std::vector<int> vektor(n);
+    for(int i=0; i<n; i++) {
+        while(!(std::cin >> vektor.at(i))){
+            std::cout << "Pogresan unos!\n";
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
+    }
+    std::vector<bool> rez = Suma5UzastopnihBrojeva(vektor);
+    for(int x : rez) std::cout << std::boolalpha << x << " ";
+    std::cout << std::endl;
+    return 0;
+}
